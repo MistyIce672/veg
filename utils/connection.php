61693b787a6925ad250c938ea48d5a922659530b
Gautham Kulasingham <67894395+MistyIce672@users.noreply.github.com>
2025-03-14 17:41:15 +0530

    Update connection.php

--- utils/connection.php
+++ utils/connection.php
@@ -3,11 +3,7 @@ $servername = "db";
 $username = "user";
 $password = "1qaz2wsx";
 $db_name = "veg";
-<<<<<<< HEAD
-$conn = new mysqli($servername, $username, $password, $db_name);
-=======
 $conn = new mysqli($servername, $username, $password, $db_name, 3306);
->>>>>>> main
 if ($conn->connect_error) {
     die("connection failed" . $conn->connect_error);
 }
